47c210066b1e6cbeefb46ecc4c0555a694bd2dc4
Cyrus <csarkosh@uw.edu>
2016-08-05 23:07:56 -0700

    changed on success method to once success method for mongo connection

--- routes/campaignsRoute.js
+++ routes/campaignsRoute.js
@@ -8,13 +8,13 @@ var db = mongoose.connection;
 
 // handle connection error
 db.on('error', function() {
-	console.log('oops... something went wrong');
+	console.log('oops... something went wrong in campaigns');
 	mongoose.disconnect();
 	return;
 });
 
 // handle connection open
-db.on('open', function() {
+db.once('open', function() {
 	router.route('/')
 		.get(function(req, res) {
 			res.send('connection established. get worked');
