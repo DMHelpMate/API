8f291ccc03d441824e222e41af186c20a2000798
Cyrus <csarkosh@uw.edu>
2016-08-05 21:55:39 -0700

    Setup mongodb connection on get and post requests

--- routes/campaignsRoute.js
+++ routes/campaignsRoute.js
@@ -1,8 +1,26 @@
 var router = require('express').Router();
+var mongoose = require('mongoose');
 
 router.route('/')
 	.get(function(req, res) {
-		res.send('Campaigns works!');
+		mongoose.connect('mongodb://localhost/unicorn');
+		var db = mongoose.connection;
+		db.on('error', function() {
+			res.send('oops... something went wrong');
+		});
+		db.on('open', function() {
+			res.send('get worked. mongo connection worked.');
+		});
+	})
+	.post(function(req, res) {
+		mongoose.connect('mongodb://localhost/unicorn');
+		var db = mongoose.connection;
+		db.on('error', function() {
+			res.send('oops... something went wrong');
+		});
+		db.on('open', function() {
+			res.send('get worked. mongo connection worked.');
+		});
 	});
 
 module.exports = router;
