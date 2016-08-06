7a2faadc614a4c1f21f6dd3fe0c57d93b89c67fd
Cyrus <csarkosh@uw.edu>
2016-08-05 22:46:40 -0700

    refactored mongoose scope to nest express routes. Closed db connection after route req

--- routes/monstersRoute.js
+++ routes/monstersRoute.js
@@ -2,26 +2,30 @@
 var router = require('express').Router();
 var mongoose = require('mongoose');
 
-router.route('/')
-	.get(function(req, res) {
-		mongoose.connect('mongodb://localhost/unicorn');
-		var db = mongoose.connection;
-		db.on('error', function() {
-			res.send('oops... something went wrong');
+// initialize connection
+mongoose.connect('mongodb://localhost/unicorn');
+var db = mongoose.connection;
+
+// handle connection error
+db.on('error', function() {
+	console.log('oops... something went wrong');
+	mongoose.disconnect();
+	return;
+});
+
+// handle connection open
+db.on('open', function() {
+	router.route('/')
+		.get(function(req, res) {
+			res.send('connection established. get worked');
+			mongoose.disconnect();
+			return;
+		})
+		.post(function(req, res) {
+			res.send('connection established. get worked');
+			mongoose.disconnect();
+			return;
 		});
-		db.on('open', function() {
-			res.send('get worked. mongo connection worked.');
-		});
-	})
-	.post(function(req, res) {
-		mongoose.connect('mongodb://localhost/unicorn');
-		var db = mongoose.connection;
-		db.on('error', function() {
-			res.send('oops... something went wrong');
-		});
-		db.on('open', function() {
-			res.send('get worked. mongo connection worked.');
-		});
-	});
+});
 
 module.exports = router;
