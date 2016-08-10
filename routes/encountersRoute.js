a5a6d943cf418da624726053281cc54cc24ed790
Cyrus <csarkosh@uw.edu>
2016-08-10 15:25:46 -0700

    Added POST request function

--- routes/encountersRoute.js
+++ routes/encountersRoute.js
@@ -4,22 +4,35 @@ var router = require('express').Router();
 
 // mongoose vars
 var mongoose,
-	encountersSchema;
+	Encounter;
 
 // route http reqs
 router.use(function(req, res, next) {
 		mongoose = req.app.get('mongoose');
+		Encounter = mongoose.model('Encounter', req.app.get('EncountersSchema'));
 		next();
 	})
 	.route('/')
 		.delete(function(req, res) {
-			res.send('Accessed DELETE req');
+			res.sendStatus(403);
 		})
 		.get(function(req, res) {
 			res.send('Accessed GET req');
 		})
 		.post(function(req, res) {
-			res.send('Accessed POST req');
+			if (req.body) {
+				Encounter.create(req.body, function(err, newEncounter) {
+					if (err) {
+						console.log('/encounters POST request: Error: ')
+						console.log(err);
+					} else {
+						console.log('/encounters POST request: OK')
+					}
+				})
+			} else {
+				console.log('/encounters POST request: Error: ');
+				console.log('null request body');
+			}
 		})
 		.put(function(req, res) {
 			res.send('Accessed PUT req');
