2e8b551e3af398a624d6431176aef38d516c7d4b
Cyrus <csarkosh@uw.edu>
2016-08-13 15:20:29 -0700

    retrieves all monsters when enc_id specified

--- routes/encountersRoute.js
+++ routes/encountersRoute.js
@@ -1,18 +1,65 @@
 
 // import dependencies
 var router = require('express').Router();
+var http = require('http');
 
 // query select
-const SELECT = '-_id general location monsters'
+const SELECT = '-_id general location monsters';
+const MON_SELECT = '-_id mname mhitpoints mattack mdefense';
 
 // mongoose vars
 var mongoose,
-	Encounter;
+	Encounter,
+	Monster;
+
+
+/**
+ * getMs() retrieves all monsters from the encounter's monsters[]
+ *
+ * @param {object} enc An encounter that contains a monsters[]
+ */
+ // function getMs(enc, callback) {
+ // 	var fullResult = {enc: {}, monsters: []};
+ // 	var isUsed = false;
+	// fullResult.enc = enc;
+ // 	for (var i = 0; i < enc.monsters.length;) {
+ // 		console.log(i + " == " + enc.monsters.length);
+ // 		if (!isUsed) {
+ // 			isUsed = true;
+ // 			i += 1;
+	//  		Monster.findOne({'mon_id': enc.monsters[i - 1].mon_id}, MON_SELECT, function(err, monResult) {
+	//  			console.log('hello');
+	//  			if (!err)
+	//  				fullResult.monsters.push(monResult);
+	//  			isUsed = false;
+	//  			if (i == enc.monsters.length) {
+	//  				callback(fullResult);
+	//  			}
+	//  		});
+ // 		}
+ // 	}
+ // }
+function getMs(enc, callback) {
+	var fullResult = {enc: {}, monsters: []};
+	fullResult.enc = enc;
+	for (var i = 0; i < enc.monsters.length; i++) {
+		(function(i) {
+			Monster.findOne({'mon_id': enc.monsters[i].mon_id}, MON_SELECT, function(err, monResult) {
+				fullResult.monsters.push(monResult);
+				if (i == enc.monsters.length - 1) {
+					callback(fullResult);
+				}
+			});
+		}(i));
+	}
+}
+
 
 // route http reqs
 router.use(function(req, res, next) {
 		mongoose = req.app.get('mongoose');
 		Encounter = mongoose.model('Encounter', req.app.get('EncountersSchema'));
+		Monster = mongoose.model('Monster', req.app.get('MonstersSchema'));
 		res.header('Access-Control-Allow-Origin', '*');
 		res.header('POST, GET');
 		res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
@@ -29,7 +76,9 @@ router.use(function(req, res, next) {
 					if (err) {
 						return res.status(500).json(null);
 					} else {
-						return res.status(200).json(result);
+						getMs(result, function(newResult) {
+							return res.status(200).json(newResult);
+						});
 					}
 				});
 			}
@@ -46,6 +95,7 @@ router.use(function(req, res, next) {
 		})
 		.post(function(req, res) {
 			if (req.body) {
+				req.body._id = mongoose.Types.ObjectId(req.body.enc_id);
 				Encounter.create(req.body, function(err, newEncounter) {
 					if (err) {
 						console.log('/encounters POST request: Error: ')
