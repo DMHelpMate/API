153b06d5a5adddee7e4c187beb4a733f81155bf4
Cyrus <csarkosh@uw.edu>
2016-08-13 23:37:41 -0700

    added cache-control in middleware

--- routes/campaignsRoute.js
+++ routes/campaignsRoute.js
@@ -44,6 +44,7 @@ router.use(function(req, res, next) {
 		res.header('Access-Control-Allow-Origin', '*');
 		res.header('POST, GET');
 		res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+		res.header('Cache-Control', 'public, max-age=31557600');
 		next();
 	})
 	.route('/')
