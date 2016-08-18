868e46fc65203ced4f8a3bf53553d049ac48abcc
Cyrus <csarkosh@uw.edu>
2016-08-17 18:41:21 -0700

    added mysql connection

--- app.js
+++ app.js
@@ -4,19 +4,40 @@ var express 	= require('express');
 var app 		= express();
 var mongoose 	= require('mongoose');
 var bodyParser 	= require('body-parser');
+var mysql		= require('mysql');
 
 // it's a cool header for the / route
 const COOL_HEADER = 
 ' _    _       _                      _____                                                     _____ _____ \n| |  | |     (_)                    |  __ \\                                              /\\   |  __ \\_   _|\n| |  | |_ __  _  ___ ___  _ __ _ __ | |__) |__ _ _ __ ___  _ __   __ _  __ _  ___       /  \\  | |__) || |  \n| |  | | \'_ \\| |/ __/ _ \\| \'__| \'_ \\|  _  // _` | \'_ ` _ \\| \'_ \\ / _` |/ _` |/ _ \\     / /\\ \\ |  ___/ | |  \n| |__| | | | | | (_| (_) | |  | | | | | \\ \\ (_| | | | | | | |_) | (_| | (_| |  __/    / ____ \\| |    _| |_ \n \\____/|_| |_|_|\\___\\___/|_|  |_| |_|_|  \\_\\__,_|_| |_| |_| .__/ \\__,_|\\__, |\\___|   /_/    \\_\\_|   |_____|\n                                                          | |           __/ |                              \n                                                          |_|          |___/                               \n';
 
-// create db connection and set vars for routes to access db
+// create mongodb connection and set vars for routes to access mongodb
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://127.0.0.1/unicorn');
 var db = mongoose.connect;
 app.set('db', db);
 app.set('mongoose', mongoose);
 
-// globalize schemas
+// create mysql connection and set vars for routes to access mysql
+var mysqlConn = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'password',
+	database: 'unicorn'
+});
+mysqlConn.connect(function(err) {
+	if (err) {
+		console.log('MySQL: ERROR in connect: ');
+		console.log(err);
+		app.set('mysql', null);
+		app.set('mysqlConn', null);
+	} else {
+		console.log('MySQL: connection OK');
+		app.set('mysql', mysql);
+		app.set('mysqlConn', mysqlConn);
+	}
+});
+
+// globalize mongo schemas
 app.set('MonstersSchema', require('./schemas/monstersSchema'));
 app.set('EncountersSchema', require('./schemas/encountersSchema'));
 app.set('CampaignsSchema', require('./schemas/campaignsSchema'));
